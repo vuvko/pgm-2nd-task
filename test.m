9f7603806471f23a352827ef05d3ea963e6ea0f2
Andrei Shadrikov <6129086+vuvko@users.noreply.github.com>
2014-03-22 16:30:12 +0400

    ????????? ??????? ?????????????.

--- test.m
+++ test.m
@@ -1,17 +1,29 @@
 clear;
 
 n = 256;
-m = 128;
-k = n - m;
+k = 32;
+m = n - k;
 %q = rand(1) / 2; % q --- random
 q = 0.25;        % q --- fixed 
 H = make_ldpc_mex(m, n, 3);
 % проверяем правильность построения порождающей матрицы mod(H * G, 2) == 0
-[G, ind] = ldpc_gen_matrix(H);
-assert(sum(sum(G(ind, :) ~= eye(k))) == 0);
-assert(sum(sum(mod(H * G, 2) ~= 0)) == 0);
-disp 'ldpc_gen_matrix checked.'
+%[G, ind] = ldpc_gen_matrix(H);
+%assert(sum(sum(G(ind, :) ~= eye(k))) == 0);
+%assert(sum(sum(mod(H * G, 2) ~= 0)) == 0);
+%disp 'ldpc_gen_matrix checked.'
 
-e = binornd([1:n]', q);
-v = rand(n, 1);
-w = v + e;
+display(['Channel ', num2str(1 + q * log2(q) + (1 - q) * log2(1 - q))]);
+display(['Speed ', num2str(k / n)]);
+e = mod(binornd([1:n]', q), 2);
+%v = randi(2, n, 1) - 1;
+v = ones(n, 1);
+w = xor(v, e);
+s = mod(H * w, 2);
+[e_n, status] = ldpc_decoding(s, H, q);
+if status == 2
+    disp 'Max iter.';
+else
+    disp 'Good';
+    sum(e ~= e_n)
+    max(e_n)
+end
